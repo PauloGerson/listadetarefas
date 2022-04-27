df204b7cbfbd72e57c0493b1598cff1453ddad38
PauloGerson <35962308+PauloGerson@users.noreply.github.com>
2022-04-27 14:57:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let container = document.querySelector('.tarefa');
+let btn = document.querySelector('.btn');
+let input = document.querySelector('.input');
+
+btn.addEventListener('click', function(e){
+    console.log('oi')
+    criaLi();
+    
+})
+
+function criaLi(){
+    let li = document.createElement('li');
+    li.innerText = input.value;
+    //criarbtn(li);
+    criarIcone(li);
+    container.appendChild(li);
+    input.value = "";
+    return li;
+}
+
+
+  function criarbtn(li){
+    let btn = document.createElement('button');
+    btn.innerHTML = 'apagar';
+    li.appendChild(btn);
+    
+}  
+
+function criarIcone(li){
+    let icon = document.createElement('i');
+    icon.setAttribute('class', 'fa-solid fa-trash');
+    li.appendChild(icon);
+}
+
