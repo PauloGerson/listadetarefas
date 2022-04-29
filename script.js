0fb91f8a0262e66545d5c4e6bd45d96bd478d4fe
PauloGerson <35962308+PauloGerson@users.noreply.github.com>
2022-04-29 09:06:37 -0300

    adicionado a funcionalidade de completar tarefa

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
 let container = document.querySelector('.tarefa');
 let btn = document.querySelector('.btn');
 let input = document.querySelector('.input');
+let li;
 
 btn.addEventListener('click', function(e){
     console.log('oi')
-    criaLi();
-    
+    criaLi();  
 })
 
 function criaLi(){
-    let li = document.createElement('li');
+    li = document.createElement('li');
     li.innerText = input.value;
-    //criarbtn(li);
-    criarIcone(li);
+    apagarLi(li);
+    completarLi(li)
     container.appendChild(li);
     input.value = "";
     return li;
 }
 
-function criarIcone(li){
-    let icon = document.createElement('i');
-    icon.setAttribute('class', 'fa-solid fa-trash');
-    li.appendChild(icon);
-}
-
-function criarIcone(li){
+function apagarLi(li){
     let icon = document.createElement('i');
     icon.setAttribute('class', 'fa-solid fa-trash');
     li.appendChild(icon);
@@ -32,6 +26,16 @@ function criarIcone(li){
     icon.addEventListener('click', function(e){
         li = e.target;
         li.parentNode.remove();
+    })   
+}
+
+function completarLi(li){
+    li.addEventListener('click', function(e){
+        li = e.target;
+       li.classList.toggle('visible');
+       
     })
 }
+
+
 
