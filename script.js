934c51ee603c795c3d74a66d4e55c39c8e9aae75
PauloGerson <35962308+PauloGerson@users.noreply.github.com>
2022-05-04 04:29:33 -0300

    resolvendo bugs de item em branco

--- script.js
+++ script.js
@@ -4,28 +4,35 @@ let input = document.querySelector('.input');
 let li;
 
 btn.addEventListener('click', function(e){
-    console.log('oi')
-    criaLi();  
+    //console.log('oi') 
+    criaLi(); 
+ 
 })
 
 function criaLi(){
+    if(input.value !== ''){
     li = document.createElement('li');
     li.innerText = input.value;
     apagarLi(li);
     completarLi(li)
     container.appendChild(li);
-    input.value = "";
-    return li;
+    salvarTarefa()
+    input.value = '';
+    
+}
 }
 
 function apagarLi(li){
     let icon = document.createElement('i');
+    let span = document.createElement('span');
     icon.setAttribute('class', 'fa-solid fa-trash');
-    li.appendChild(icon);
+    span.appendChild(icon);
+    li.appendChild(span);
 
     icon.addEventListener('click', function(e){
         li = e.target;
-        li.parentNode.remove();
+        li.parentNode.parentNode.remove();
+        salvarTarefa()
     })   
 }
 
@@ -36,6 +43,37 @@ function completarLi(li){
        
     })
 }
+
+
+//selecionado as li para salvar no localStorage
+function salvarTarefa(){
+    const liTarefas = container.querySelectorAll('li');
+    const listaDeTarefas = [];
+
+    for (let tarefa of liTarefas){
+        let text = tarefa.innerText
+        listaDeTarefas.push(text); // com o push, estamos adicionando os itens de tarefas em listaDetarfas
+    } 
+
+    const tarefaJSON = JSON.stringify(listaDeTarefas); // formtando um array para um tipo string do obejto json
+    localStorage.setItem('tarefas', tarefaJSON);
+    
+}
+
+function adicionarTarefasSalvar(){
+    const tarefas = localStorage.getItem('tarefas');
+    const listaDeTarefas = JSON.parse(tarefas); // voltando a seu tipo inicial, no caso um array de caracteres
+    
+    for(let tarefa of listaDeTarefas){
+        criaLi(tarefa);
+    }
+}
+
+adicionarTarefasSalvar();
+
+
+
+
 
 
 
